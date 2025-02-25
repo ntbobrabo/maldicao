04275fa4d13925a93e0ccb3bb53ef8c761eb905e
ntbobrabo <vinylobrabo@gmail.com>
2025-02-25 11:42:14 -0300

    feat: verifica se é maior, menor ou igual

---
+++ src/aula04/C007.java
@@ -0,0 +1,32 @@
+package aula04;
+
+/*
+C007 Faça um algoritmo que leia dois números quaisquer e informe se o primeiro é menor, é
+igual ou maior que o segundo número digitado.
+ */
+
+import java.util.Scanner;
+
+public class C007 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Número 1: ");
+
+        int numero1 = scanner.nextInt();
+        System.out.println("Número 2: ");
+
+        int numero2 = scanner.nextInt();
+
+        if (numero1 < numero2){
+            System.out.println("O número "+numero1+ " é menor que "+numero2);
+
+        } else if (numero1 == numero2){
+            System.out.println(("O número "+numero1+ " é igual ao "+numero2));
+
+        } else{
+            System.out.println("O número "+numero1+ "é maior que "+numero2);
+
+        }
+    }
+}
